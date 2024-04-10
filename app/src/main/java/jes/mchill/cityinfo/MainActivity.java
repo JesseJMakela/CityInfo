5eae700220ae4dc156c1e68060307e0d714eeee6
JesseJMakela <jesse.j.makela@gmail.com>
2024-04-10 16:19:55 +0300

    Lisätty fragmentit A, B ja Cmutta main activityyn ei pääse enää takaisin.

--- app/src/main/java/jes/mchill/cityinfo/MainActivity.java
+++ app/src/main/java/jes/mchill/cityinfo/MainActivity.java
@@ -4,14 +4,20 @@ import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import java.util.ArrayList;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+
+import jes.mchill.cityinfo.fragments.FragmentA;
+import jes.mchill.cityinfo.fragments.FragmentB;
+import jes.mchill.cityinfo.fragments.FragmentC;
 
 public class MainActivity extends AppCompatActivity {
     private TextView txtPopulationData;
@@ -28,8 +34,37 @@ public class MainActivity extends AppCompatActivity {
         txtPopulationData = findViewById(R.id.txtPopulation);
         txtWeatherData = findViewById(R.id.txtWeather);
         editTextLocation = findViewById(R.id.txtEditLocation);
+
+        Button fragmentA = findViewById(R.id.btnA);
+        Button fragmentB = findViewById(R.id.btnB);
+        Button fragmentC = findViewById(R.id.btnC);
+
+        fragmentA.setOnClickListener(listener);
+        fragmentB.setOnClickListener(listener);
+        fragmentC.setOnClickListener(listener);
+
+
     }
 
+    private View.OnClickListener listener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Fragment fragment;
+
+            if (view.getId() == R.id.btnA) {
+                fragment = new FragmentA();
+            } else if (view.getId() == R.id.btnB) {
+                fragment = new FragmentB();
+            } else if (view.getId() == R.id.btnC) {
+                fragment = new FragmentC();
+            } else {
+                fragment = new FragmentA();
+            }
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.frame, fragment)
+                    .commit();
+        }
+    };
     public void onFindBtnClick(View view) {
         Log.d("Lut", "Nappula toimii");
         Context context = this;
